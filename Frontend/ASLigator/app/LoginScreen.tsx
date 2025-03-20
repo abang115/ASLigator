eb97aafc19923aa09ad0cfac0d3b668feca832f3
Alvin Bang <alvin.chijunbang@gmail.com>
2025-03-20 19:59:23 -0400

    Added form validation on login and register

--- Frontend/ASLigator/app/LoginScreen.tsx
+++ Frontend/ASLigator/app/LoginScreen.tsx
@@ -1,26 +1,36 @@
 import { KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native'
 import React, {useState} from 'react'
+import { useForm, Controller } from "react-hook-form";
 import auth from "@react-native-firebase/auth"
 import { useRouter } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons';
+
+interface FormData {
+  email: string;
+  password: string;
+}
  
 export default function LoginScreen() {
   const router = useRouter()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
+      defaultValues: {
+        email: "",
+        password: "",
+      }
+    })
 
-  const login = async () => {
-    if (email && password) {
+  const login = async (data: FormData) => {
+    if (data.email && data.password) {
       try {
         const response = await auth().signInWithEmailAndPassword(
-          email,
-          password
+          data.email,
+          data.password
         );
         if (response.user) {
           router.push("/HomeScreen")
         }
-      } catch (e) {
-        alert("Error!")
+      } catch (e: any) {
+        alert("Incorrect email or password. Please try again!")
       }
     }
   };
@@ -36,25 +46,52 @@ export default function LoginScreen() {
         <Text style={styles.headerText}>Login</Text>
         <View style={styles.inputContainer}>
             <Text style={styles.label}>Email</Text>
-            <TextInput
-                placeholder="Enter your email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                style={styles.input}
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+              required: "Email is required.",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Invalid email address.",
+                },
+               }}
+              render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={[styles.input, errors.email && styles.errorInput]}
+                placeholder="janedoe@email.com"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                />
+              )}
             />
+            {errors.email && <Text style={styles.errorText}>{errors.email.message}</Text>}
             <Text style={styles.label}>Password</Text>
-            <TextInput
-                placeholder="Enter your password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                style={styles.input}
-                secureTextEntry
-            />
+            <Controller
+              control={control}
+              name="password"
+              rules={{
+                required: "Password is required.",
+                minLength: { value: 6, message: "Minimum 6 characters required." },
+              }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  style={[styles.input, errors.password && styles.errorInput]}
+                  placeholder="Minimum 6 characters"
+                  secureTextEntry
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  />
+                )}
+              />
+              {errors.password && <Text style={styles.errorText}>{errors.password.message}</Text>}
         </View>
 
         <View style={styles.buttonContainer}>
           <TouchableOpacity
-            onPress={ login }
+            onPress={ handleSubmit(login) }
             style={styles.button}
           >
           <Text style={styles.buttonText}>Login</Text>
@@ -89,8 +126,9 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     backgroundColor: "#fff",
     borderRadius: 10,
+    borderWidth: 1,
     marginTop: 5,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   buttonContainer: {
     width: "60%",
@@ -136,5 +174,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#33418b",
     fontWeight: "bold",
+  },
+  errorInput: { 
+    borderColor: "red" 
+  },
+  errorText: { 
+    color: "red",
+    marginTop: -20,
   },
 })
--- Frontend/ASLigator/app/RegisterScreen.tsx
+++ Frontend/ASLigator/app/RegisterScreen.tsx
@@ -1,50 +1,66 @@
 import { KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native'
-import React, {useState} from 'react'
+import { useForm, Controller } from "react-hook-form";
+import React from 'react'
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth"
 import db from "@react-native-firebase/database"
 import firestore from "@react-native-firebase/firestore"
 import { useRouter } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons';
 
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 export default function RegisterScreen() {
   const router = useRouter()
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const { control, handleSubmit, formState: { errors }, watch } = useForm<FormData>({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    }
+  })
 
-  const createProfile = async (response: FirebaseAuthTypes.UserCredential) => {
-    db().ref(`/users/${response.user.uid}`).set({ firstName, lastName });
+  const createProfile = async (response: FirebaseAuthTypes.UserCredential, data: FormData) => {
+    db().ref(`/users/${response.user.uid}`).set({ 
+      firstName: data.firstName,
+      lastName: data.lastName,
+     });
   };
 
-  const register = async () => {
-    if(password != confirmPassword) {
-      alert("Passwords do not match!")
-    }
-    else {
-      if (email && password) {
+  const register = async (data: FormData) => {
+      if (data.email && data.password) {
         try {
           const response = await auth().createUserWithEmailAndPassword(
-            email,
-            password
+            data.email,
+            data.password
           );
           if (response.user) {
-            await createProfile(response)
+            await createProfile(response, data)
             await firestore().collection("users").doc(response.user.uid).set({
-              firstName: firstName,
-              lastName: lastName,
-              email: email,
+              firstName: data.firstName,
+              lastName: data.lastName,
+              email: data.email,
               profilePicture: "",
               createdAt: firestore.FieldValue.serverTimestamp(),
             });
             router.push("/HomeScreen")
           }
-        } catch (e) {
-          alert("There was an error creating your account.")
+        } catch (e: any) {
+          if(e.code === "auth/email-already-in-use") {
+            alert("That email is already in use.")
+          }
+          else {
+            alert("There was an error creating your account.")
+          }
         }
       }
-    }
   };
 
   return (
@@ -57,48 +73,110 @@ export default function RegisterScreen() {
       </TouchableOpacity>
       <Text style={styles.headerText}>Register</Text>
       <View style={styles.inputContainer}>
-          <Text style={styles.label}>First Name</Text>
-          <TextInput
+        <Text style={styles.label}>First Name</Text>
+        <Controller
+          control={control}
+          name="firstName"
+          rules={{ required: "First name is required." }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[styles.input, errors.firstName && styles.errorInput]}
               placeholder="Ex: Jane"
-              value={firstName}
-              onChangeText={text => setFirstName(text)}
-              style={styles.input}
-          />
-          <Text style={styles.label}>Last Name</Text>
-          <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.firstName && <Text style={styles.errorText}>{errors.firstName.message}</Text>}
+
+        <Text style={styles.label}>Last Name</Text>
+        <Controller
+          control={control}
+          name="lastName"
+          rules={{ required: "Last name is required." }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[styles.input, errors.lastName && styles.errorInput]}
               placeholder="Ex: Doe"
-              value={lastName}
-              onChangeText={text => setLastName(text)}
-              style={styles.input}
-          />
-          <Text style={styles.label}>Email</Text>
-          <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.lastName && <Text style={styles.errorText}>{errors.lastName.message}</Text>}
+
+        <Text style={styles.label}>Email</Text>
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: "Email is required.",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Invalid email address.",
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[styles.input, errors.email && styles.errorInput]}
               placeholder="janedoe@email.com"
-              value={email}
-              onChangeText={text => setEmail(text)}
-              style={styles.input}
-          />
-          <Text style={styles.label}>Password</Text>
-          <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.email && <Text style={styles.errorText}>{errors.email.message}</Text>}
+
+        <Text style={styles.label}>Password</Text>
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: "Password is required.",
+            minLength: { value: 6, message: "Minimum 6 characters required." },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[styles.input, errors.password && styles.errorInput]}
               placeholder="Minimum 6 characters"
-              value={password}
-              onChangeText={text => setPassword(text)}
-              style={styles.input}
               secureTextEntry
-          />
-          <Text style={styles.label}>Confirm Password</Text>
-          <TextInput
-              placeholder="Password must match"
-              value={confirmPassword}
-              onChangeText={text => setConfirmPassword(text)}
-              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.password && <Text style={styles.errorText}>{errors.password.message}</Text>}
+
+        <Text style={styles.label}>Confirm Password</Text>
+        <Controller
+          control={control}
+          name="confirmPassword"
+          rules={{
+            required: "Please confirm your password.",
+            validate: (value) =>
+              value === watch("password") || "Passwords do not match.",
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[styles.input, errors.confirmPassword && styles.errorInput]}
+              placeholder="Re-enter password"
               secureTextEntry
-          />
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.confirmPassword && <Text style={styles.errorText}>{errors.confirmPassword.message}</Text>}
       </View>
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity
-          onPress={ register }
+          onPress={ handleSubmit(register) }
           style={styles.button}          >
         <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
@@ -133,6 +211,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderRadius: 10,
     marginBottom: 20,
+    borderWidth: 1,
   },
   buttonContainer: {
     width: "60%",
@@ -189,5 +268,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#33418b",
     fontWeight: "bold",
+  },
+  errorInput: { 
+    borderColor: "red" 
+  },
+  errorText: { 
+    color: "red",
+    marginTop: -20,
   },
 })
