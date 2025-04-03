2bb57cc1f0414a962fe38afb8e32e15a2a3ce79e
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2025-04-03 14:13:47 -0400

    Made video to text detection into a function

--- Backend/src/Video_to_Text.py
+++ Backend/src/Video_to_Text.py
@@ -1,11 +1,15 @@
 import cv2
 import json
 import os
-import joblib
+import sys
 import mediapipe as mp
 import numpy as np
 from keras.api.models import load_model
 
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(root_path)
+
+from data.helper import draw_landmarks, extract_landmarks
 
 def load_trained_model():
     # Load pre-trained model
@@ -30,124 +34,96 @@ def prob_viz(res, actions, input_frame, colors):
         cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     return output_frame
 
-# Helper: Draw landmarks
-def draw_landmarks(image, results, mp_drawings):
-    # Draw left hand connections
-    mp_drawings.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_drawings.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2),
-                               mp_drawings.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=1))
-    # Draw right hand connectionsz
-    mp_drawings.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_drawings.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2),
-                               mp_drawings.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=1))
-    # Draw face connections
-    mp_drawings.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                               mp_drawings.DrawingSpec(color=(255,0,255), thickness=1, circle_radius=1),
-                               mp_drawings.DrawingSpec(color=(0,255,0), thickness=1, circle_radius=1))
-    # Draw pose connections
-    mp_drawings.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                               mp_drawings.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2),
-                               mp_drawings.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=1))
+def video_to_text():
+    # Visualization colors
+    colors = [
+        (245, 117, 16), (117, 245, 16), (16, 117, 245),
+        (0, 117, 245), (16, 0, 245), (0, 245, 16),
+        (50, 100, 245), (180, 117, 26), (216, 0, 245), (100, 50, 245)
+    ]
 
-# Helper: Extract keypoints
-def extract_landmarks(results):
-    left_lm, right_lm, face_lm, pose_lm = [],[],[],[]
-    # Grab coords for each left hand landmark
-    left_lm = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    # Right hand landmark
-    right_lm = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    # Face landmark
-    face_lm = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
-    # Pose Landmark
-    pose_lm = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-    return np.concatenate([pose_lm, face_lm, right_lm, left_lm])
+    # Path to video from front end
+    FRONTEND_VIDEO = os.path.join(os.getcwd(), 'test.mp4')
+    print(os.getcwd())
+    # Mediapipe holistic setup
+    mp_holistic = mp.solutions.holistic
+    mp_drawings = mp.solutions.drawing_utils
+    mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-# Visualization colors
-colors = [
-    (245, 117, 16), (117, 245, 16), (16, 117, 245),
-    (0, 117, 245), (16, 0, 245), (0, 245, 16),
-    (50, 100, 245), (180, 117, 26), (216, 0, 245), (100, 50, 245)
-]
+    # Load up tained model and each action
+    model, actions = load_trained_model()
 
-# Path to video from front end
-FRONTEND_VIDEO = os.path.join(os.getcwd(), 'Test.mp4')
+    # Webcam setup
+    webcam = cv2.VideoCapture(FRONTEND_VIDEO)
 
-# Mediapipe holistic setup
-mp_holistic = mp.solutions.holistic
-mp_drawings = mp.solutions.drawing_utils
-mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    # Constants
+    width = 1280
+    height = 720
+    webcam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    sequence, sentence, predictions = [], [], []
+    threshold = 0.5
 
-# Load up tained model and each action
-model, actions = load_trained_model()
 
-# Webcam setup
-webcam = cv2.VideoCapture(FRONTEND_VIDEO)
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        frame_count = 0
+        while webcam.isOpened():
+            ret, frame = webcam.read()
+            if not ret:
+                break
 
-# Constants
-width = 1280
-height = 720
-webcam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-BAR_HEIGHT = 50
-sequence, sentence, predictions = [], [], []
-threshold = 0.5
+            frame_count += 1
 
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    frame_count = 0
-    while webcam.isOpened():
-        ret, frame = webcam.read()
-        if not ret:
-            break
+            # Skip the first few frames for 
+            if frame_count <= 5:
+                continue
 
-        frame_count += 1
+            # Process frame
+            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = holistic.process(image_rgb)
+            draw_landmarks(frame, results, mp_holistic, mp_drawings)
 
-        # Skip the first few frames for 
-        if frame_count <= 5:
-            continue
+            # Extract keypoints
+            keypoints = extract_landmarks(results)
+            sequence.append(keypoints)
+            sequence = sequence[-30:]
+            
+            text = "Awaiting Gesture..."
+            # Prediction logic
+            if len(sequence) == 30:
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                pred_index = np.argmax(res)
+                text = actions[pred_index]
+                predictions.append(pred_index)
 
-        # Process frame
-        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = holistic.process(image_rgb)
-        draw_landmarks(frame, results, mp_drawings)
+                # Smoothing, make sure last 10 predictions all match and that the prediction % is past the threshhold
+                if np.unique(predictions[-10:])[0] == pred_index and res[pred_index] > threshold:
+                    
+                    if not sentence or actions[pred_index] != sentence[-1]:
+                        sentence.append(actions[pred_index])
+                    
+                    if len(sentence) > 7:
+                        sentence = sentence[-8:]
 
-        # Extract keypoints
-        keypoints = extract_landmarks(results)
-        sequence.append(keypoints)
-        sequence = sequence[-30:]
-        
-        text = "Awaiting Gesture..."
-        # Prediction logic
-        if len(sequence) == 30:
-            res = model.predict(np.expand_dims(sequence, axis=0))[0]
-            pred_index = np.argmax(res)
-            text = actions[pred_index]
-            predictions.append(pred_index)
+                frame = prob_viz(res, actions, frame, colors)
 
-            # Smoothing, make sure last 10 predictions all match and that the prediction % is past the threshhold
-            if np.unique(predictions[-10:])[0] == pred_index and res[pred_index] > threshold:
-                
-                if not sentence or actions[pred_index] != sentence[-1]:
-                    sentence.append(actions[pred_index])
-                
-                if len(sentence) > 7:
-                    sentence = sentence[-8:]
+            cv2.rectangle(frame, (0,0), (width, 40), (245, 117, 16), -1)
+            cv2.putText(frame, ' '.join(sentence), (3,30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-            frame = prob_viz(res, actions, frame, colors)
+            # Show window
+            cv2.imshow('Hand Tracking', frame)
 
-        cv2.rectangle(frame, (0,0), (width, 40), (245, 117, 16), -1)
-        cv2.putText(frame, ' '.join(sentence), (3,30), 
-                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # Exit conditions
+            if cv2.waitKey(1) == ord('q'):
+                break
+            if cv2.getWindowProperty('Hand Tracking', cv2.WND_PROP_VISIBLE) < 1:
+                break
 
-        # Show window
-        cv2.imshow('Hand Tracking', frame)
+    webcam.release()
+    cv2.destroyAllWindows()
+    return sentence
 
-        # Exit conditions
-        if cv2.waitKey(1) == ord('q'):
-            break
-        if cv2.getWindowProperty('Hand Tracking', cv2.WND_PROP_VISIBLE) < 1:
-            break
-
-webcam.release()
-cv2.destroyAllWindows()
-
-print(sentence)
+if __name__ == "__main__":
+    sentence = video_to_text()
+    print(sentence)
